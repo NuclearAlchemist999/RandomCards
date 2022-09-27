363c9d82e1abe667eb2e989d7fccab33936f1872
NuclearAlchemist999 <90280483+NuclearAlchemist999@users.noreply.github.com>
2022-09-27 18:07:36 +0200

    Added mock data in test project

---
+++ RandomCards.Tests/Repositories/CardRepositoryMockData.cs
@@ -0,0 +1,32 @@
+﻿using RandomCards.Models;
+
+namespace RandomCards.Tests.Repositories
+{
+    public class CardRepositoryMockData
+    {
+        public Game NewGame()
+        {
+            return new Game { Id = Guid.NewGuid() };
+
+        }
+        public Card NewCard()
+        {
+            return new Card { Id = Guid.NewGuid(), Name = "queen clubs" };
+        }
+
+        public CardGame NewCardInGame()
+        {
+            return new CardGame { Id = Guid.NewGuid(), CardId = NewCard().Id, GameId = NewGame().Id };
+        }
+
+        public Hand NewHand()
+        {
+            return new Hand { Id = Guid.NewGuid(), GameId = NewGame().Id };
+        }
+
+        public CardHand NewCardInHand()
+        {
+            return new CardHand { Id = Guid.NewGuid(), CardId = NewCard().Id, HandId = NewHand().Id };
+        }
+    }
+}
