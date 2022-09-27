1848de1c9b95af487f031b37a8e8eeeb3eda154e
NuclearAlchemist999 <90280483+NuclearAlchemist999@users.noreply.github.com>
2022-09-27 18:13:24 +0200

    Added some repository tests

---
+++ RandomCards.Tests/Repositories/CardRepositoryTests.cs
@@ -0,0 +1,68 @@
+﻿
+
+namespace RandomCards.Tests.Repositories
+{
+    public class CardRepositoryTests
+    {
+        CardRepository cardRepo = new CardRepository();
+        CardRepositoryMockData mockData = new CardRepositoryMockData();
+
+        [Fact]
+        public void AddGameAndGetThatGameTest()
+        {
+            // Arrange
+            var newGame = mockData.NewGame();
+
+            // Act
+            cardRepo.AddGame(newGame);
+
+            var game = cardRepo.SearchGame(newGame.Id);
+
+            // Assert
+            Assert.NotNull(game);
+            Assert.Equal(newGame.Id, game.Id);
+        }
+
+        [Fact]
+        public void AddAndGetCardsTest()
+        {
+            // Arrange 
+            cardRepo.AddCards();
+
+            //Act
+            var cards = cardRepo.GetCards();
+
+            // Assert
+            Assert.Equal(52, cards.Count());
+        }
+
+        [Fact]
+        public void AddCardToGameAndGetThatCardTest()
+        {
+            // Arrange
+            var newCardGame = mockData.NewCardInGame();
+
+            // Act
+            cardRepo.AddCardToGame(newCardGame);
+            var card = cardRepo.GetCardByGameIdAndCardId(newCardGame.GameId, newCardGame.CardId);
+
+            // Assert
+            Assert.Equal(newCardGame, card);
+            Assert.NotNull(card);
+        }
+        [Fact]
+        public void AddToHandAndGetThatHandTest()
+        {
+            // Arrange
+            var newHand = mockData.NewHand();
+
+            // Act
+            cardRepo.AddHand(newHand);
+            var hand = cardRepo.GetHand(newHand.Id);
+
+            // Assert
+            Assert.Equal(newHand, hand);
+            Assert.NotNull(hand);
+        }
+    }
+}
